437024fc16d86d2eccfd280971bf51bf8d5aaa07
macs <thuanvuvan76@gmail.com>
2024-03-25 16:10:26 +0700

    #fix: thuanvv fix depops-v4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,44 @@
+const argEnvIndex = process.argv.indexOf('--env');
+let argEnv = (argEnvIndex !== -1 && process.argv[argEnvIndex + 1]) || '';
+
+const RUN_ENV_MAP = {
+  local: {
+    instances: 2,
+    max_memory_restart: '250M',
+  },
+  dev: {
+    instances: 2,
+    max_memory_restart: '250M',
+  },
+  prod: {
+    instances: 4,
+    max_memory_restart: '1000M',
+  },
+};
+
+if (!(argEnv in RUN_ENV_MAP)) {
+  argEnv = 'prod';
+}
+
 module.exports = {
   apps: [
     {
       name: 'metaverse-website',
-      script: 'npm',
-      args: 'run dev',
-      watch: true,
-      ignore_watch: ['node_modules'],
-      instances: 1,
-      autorestart: true,
-      exec_mode: 'fork',
-      max_memory_restart: '1G',
+      exec_mode: 'cluster',
+      instances: RUN_ENV_MAP[argEnv].instances,
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      watch: false,
+      max_memory_restart: RUN_ENV_MAP[argEnv].max_memory_restart,
+      env_local: {
+        APP_ENV: 'local',
+      },
+      env_dev: {
+        APP_ENV: 'dev',
+      },
+      env_prod: {
+        APP_ENV: 'prod',
+      },
     },
   ],
 };
